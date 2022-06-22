612f371c04ea652ebbd53e38109d24a93dc715c2
hithin chandran <hithindeveloper@gmail.com>
2022-06-22 20:37:10 +0530

    22-06-2022
    night

--- js/main.js
+++ js/main.js
@@ -84,6 +84,29 @@
 				}
 			}
 		});
+		
+		$('.carousel-car_brand').owlCarousel({
+			center: true,
+			loop: true,
+			autoplay: true,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
+				},
+				1000:{
+					items: 3
+				}
+			}
+		});
+		
 		$('.carousel-testimony').owlCarousel({
 			center: true,
 			loop: true,
